be650dd558c1cb4a59fedf972b41454bd181f61e
Binary Shadow <174438598+BinaryShadow46@users.noreply.github.com>
2025-12-20 00:31:53 +0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['api.placeholder.com'],
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+module.exports = nextConfig
